80122606d29e9de016f2727b8f217a7ed77f307c
suresh3870 <sksureshkumar88@gmail.com>
2019-01-01 19:27:44 +0530

    Adding auth option

--- get_apidata.py
+++ get_apidata.py
@@ -1,13 +1,15 @@
 import requests
 
 try:
-    resp = requests.get('http://127.0.0.1:8000/scrumboard/lists/')
-
+    #resp = requests.get('http://127.0.0.1:8000/scrumboard/lists/')
+    resp = requests.get('http://127.0.0.1:8000/scrumboard/lists/',
+                            auth=('suresh', 'pali@123'))
+    code = resp.status_code
     if resp.status_code == 200:
         for i in resp.json():
-            print('{} {}'.format(i['id'], i['name']))
+            print(i)
     else:
-        print("Bad url or request")
+        print("Bad url or request or wrong password", code)
 
 except:
     print("API not rechable")
